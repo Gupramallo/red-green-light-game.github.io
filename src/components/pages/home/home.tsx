bc03ad77f69db9dd119329df993537ab4040645b
Gustavo Penalver <gustavo.penalver@ufirstgroup.ch>
2025-09-24 23:01:28 +0200

    setup mui

--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -1,7 +1,8 @@
+import { Typography } from '@mui/material'
 import type React from 'react'
 
 const Home: React.FC = () => {
-  return <>Home</>
+  return <Typography>Home</Typography>
 }
 
 export default Home
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
+import { ThemeProvider } from '@mui/material'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import theme from './styles/theme.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
 )
